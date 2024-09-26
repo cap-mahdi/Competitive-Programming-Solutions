5f2e399524de24b1239cd531a89d1c2a0cff893f
Mahdi <mahdi02ch@gmail.com>
2024-09-26 08:45:44 +0100

    Add codeforces_972_div_2

---
+++ codeforces/codeforces_972_div_2/2005A-SimplePalindrome.cpp
@@ -0,0 +1,54 @@
+/*
+Problem Link: https://codeforces.com/contest/2005/problem/A
+Created By mahdich02 on 2024-09-26
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#define int long long int
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+
+void no() {
+    cout << "NO" << "\n";
+}
+void yes() {
+    cout << "YES" << "\n";
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<char> c = { 'a','e','i','o','u' };
+    vector<char> ans;
+    for (int i = 0;i < n;i++) {
+        ans.push_back(c[i % 5]);
+    }
+    sort(ans.begin(), ans.end());
+    for (char a : ans) cout << a;
+    cout << "\n";
+
+}
+int32_t main() {
+    //fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    // online submission
+#endif
+
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/codeforces_972_div_2/2005B1-TheStrictTeacher(easyVersion).cpp
@@ -0,0 +1,72 @@
+/*
+Problem Link: https://codeforces.com/contest/2005/problem/B1
+Created By mahdich02 on 2024-09-26
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#define int long long int
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+using namespace std;
+using namespace __gnu_pbds;
+
+void no() {
+    cout << "NO" << "\n";
+}
+void yes() {
+    cout << "YES" << "\n";
+}
+
+void solve() {
+
+    int n, m, q;
+    cin >> n >> m >> q;
+    vector<int> a;
+    for (int i = 0;i < m;i++) {
+        int b;
+        cin >> b;
+        a.push_back(b);
+    }
+    sort(a.begin(), a.end());
+    while (q--) {
+
+        int x;
+        cin >> x;
+        int index = lower_bound(a.begin(), a.end(), x) - a.begin();
+        if (index == 0) {
+            cout << a[index] - 1 << "\n";
+            continue;
+        }
+        if (index == m) {
+            cout << n - a[index - 1] << "\n";
+            continue;
+        }
+        int mid = (a[index] + a[index - 1]) / 2;
+        cout << min(a[index] - mid, mid - a[index - 1]) << "\n";
+
+
+    }
+
+}
+int32_t main() {
+    //fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    // online submission
+#endif
+
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
